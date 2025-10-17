95128fa5a35389b9e6c93c7915bb7e558f66c81e
Nikhil <nikhilgelli1@gmail.com>
2025-10-17 21:21:16 +0530

    2nd commit

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-047087104d2773d13"
+ec2_instance_type = "t3.micro"
+region = "ap-south-2"
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    description = "AWS Region"
+    type = string
+}
+
+variable "ami_id" {
+    description = "ID of AMI running on the EC2 Instance"
+    type = string
+}
+
+variable "ec2_instance_type" {
+    description = "Type of the EC2 Instance"
+    type = string
+}
